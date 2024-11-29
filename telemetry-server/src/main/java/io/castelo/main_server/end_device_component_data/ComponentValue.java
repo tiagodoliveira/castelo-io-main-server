f54b7c284101353dde0fd218e48eb6258e3195f0
tiagodoliveira <tiagod_oliveira@hotmail.com>
2024-11-29 17:33:32 +0000

    Remove telemetry server and restructure gradle setup.
    The telemetry server and its associated configurations, dependencies, and components have been removed for cleaner project management. The `gradlew` script and `gradle-wrapper.jar` have been consolidated to streamline build processes. Unused `.gitignore` entries and project components specific to the telemetry server have been deleted.

--- telemetry-server/src/main/java/io/castelo/main_server/end_device_component_data/ComponentValue.java
+++
@@ -1,5 +0,0 @@
-package io.castelo.main_server.end_device_component_data;
-
-import java.time.LocalDateTime;
-
-public record ComponentValue(LocalDateTime timestamp, String value) {}
--- telemetry-server/src/main/java/io/castelo/main_server/end_device_component_data/EndDeviceComponent.java
+++
@@ -1,8 +0,0 @@
-package io.castelo.main_server.end_device_component_data;
-
-import java.util.List;
-
-public record EndDeviceComponent(
-        int componentNumber,
-        List<ComponentValue> componentValues
-) {}
