78f46a1850e18d5d5d3a205702b457cf233ca40c
tiagodoliveira <tiagod_oliveira@hotmail.com>
2024-11-29 16:02:00 +0000

    Rename MainServerApplication to TelemetryServerApplication
    The main class and its corresponding test class have been renamed to better reflect their purpose related to telemetry functions. This change ensures consistency and clarity within the codebase regarding the application's role.

--- telemetry-server/src/main/java/io/castelo/main_server/TelemetryServerApplication.java
+++ telemetry-server/src/main/java/io/castelo/main_server/TelemetryServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainServerApplication {
+public class TelemetryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MainServerApplication.class, args);
+		SpringApplication.run(TelemetryServerApplication.class, args);
 	}
 
 }
