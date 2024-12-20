d7b93a33c38a5583fc8066caeef29a9d79c09f64
tiagodoliveira <tiagod_oliveira@hotmail.com>
2024-12-20 17:16:34 +0000

    Add field exclusions to EndDeviceMapper update mapping
    Ignored mapping for specific fields (endDeviceMac, endDeviceModel, components, and user) in the updateEndDeviceFromDto method. This ensures these fields are not overwritten during updates, preserving existing values.

--- src/main/java/io/castelo/main_server/end_device/EndDeviceMapper.java
+++ src/main/java/io/castelo/main_server/end_device/EndDeviceMapper.java
@@ -1,9 +1,6 @@
 package io.castelo.main_server.end_device;
 
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -11,5 +8,9 @@ public interface EndDeviceMapper {
     EndDeviceMapper INSTANCE = Mappers.getMapper(EndDeviceMapper.class);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "endDeviceMac", ignore = true)
+    @Mapping(target = "endDeviceModel", ignore = true)
+    @Mapping(target = "components", ignore = true)
+    @Mapping(target = "user", ignore = true)
     void updateEndDeviceFromDto(EndDeviceDTO dto, @MappingTarget EndDevice entity);
 }
