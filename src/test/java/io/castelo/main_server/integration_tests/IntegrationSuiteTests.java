dde08316feb6ae245745df9c99f79b93d788f58e
tiagodoliveira <tiagod_oliveira@hotmail.com>
2025-02-22 19:46:30 +0000

    Refactor test task configuration: update Gradle build file to properly separate unit and integration tests, ensuring integration tests do not run during the default build process.

--- src/test/java/io/castelo/main_server/integration_tests/IntegrationSuiteTests.java
+++ src/test/java/io/castelo/main_server/integration_tests/IntegrationSuiteTests.java
@@ -1,13 +1,11 @@
 package io.castelo.main_server.integration_tests;
 
 
-import org.junit.jupiter.api.Tag;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
 
 @Suite
 @SelectPackages("io.castelo.main_server.integration_tests.test_classes")
-@Tag("integration")
 public class IntegrationSuiteTests {
 }
